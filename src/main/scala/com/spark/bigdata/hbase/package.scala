a470f817a896dc1d1eb83a6f19143440ab7485ef
seanshi <496458172@qq.com>
2017-09-20 11:36:32 +0800

    kafka update

--- src/main/scala/com/spark/bigdata/hbase/package.scala
+++ src/main/scala/com/spark/bigdata/hbase/package.scala
@@ -1,20 +1,23 @@
 package com.spark.bigdata
 
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.client.{Put, Result, Scan}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.client.Result
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.spark.HBaseContext
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.elasticsearch.spark._
 
 package object hbase {
 
   val spark=SparkSession.builder().appName("").master("").getOrCreate()
   val sc=spark.sparkContext
   val conf= HBaseConfiguration.create()
-  val habsecontext=new HBaseContext(sc,conf)
+  val hbaseContext=new HBaseContext(sc,conf)
+
 
 
   def scanHbaseTB(tableName:String)(implicit startKey:Option[String],endKey:Option[String]):RDD[(ImmutableBytesWritable,Result)]={
@@ -24,20 +27,31 @@ package object hbase {
         val scan=new Scan()
         scan.setStartRow(Bytes.toBytes(x))
         scan.setStopRow(Bytes.toBytes(endKey.getOrElse(x)))
-        val hbaeRDD=habsecontext.hbaseRDD(TableName.valueOf(tableName),scan)
+        val hbaeRDD=hbaseContext.hbaseRDD(TableName.valueOf(tableName),scan)
         hbaeRDD
       }
       case None=>{
         val scan=new Scan()
-        val hbaeRDD=habsecontext.hbaseRDD(TableName.valueOf(tableName),scan)
+        val hbaeRDD=hbaseContext.hbaseRDD(TableName.valueOf(tableName),scan)
         hbaeRDD
       }
     }
 
 
-    def main(args: Array[String]): Unit = {
+    def save2Hbase()={
+    }
 
+    def main(args: Array[String]): Unit = {
+      //传统方式
+      conf.set(TableInputFormat.SCAN_ROW_START, "startrowkey")
+      conf.set(TableInputFormat.SCAN_ROW_STOP, "stoprowkey")
+      conf.set(TableInputFormat.INPUT_TABLE, "SparkHbase")
+      val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result])
       val SparkHbaseRDD=scanHbaseTB("SparkHbase")
+
+
+      //利用HbaseContext进行操作
+
       SparkHbaseRDD.foreach(x=>{
         val rowKey=x._1.toString
         val rs=x._2
@@ -48,6 +62,7 @@ package object hbase {
 
     }
 
+    case class stu(sname:String,sage:Int)
 
   }
 
